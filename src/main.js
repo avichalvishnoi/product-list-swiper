7df9f04bee159b893c87be50628dd67112b9694a
avichalvishnoi <avichalvishnoi29@gmail.com>
2021-10-21 11:13:33 +0530

    new change

--- src/main.js
+++ src/main.js
@@ -4,13 +4,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 //import VuePapaParse from "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.1/papaparse.min.js"
-<<<<<<< HEAD
 import VuePapaParse from 'vue-papa-parse'
 //import VuePapaParse from 'papaparse'
-=======
-//import VuePapaParse from 'vue-papa-parse'
-import VuePapaParse from 'papaparse'
->>>>>>> 8987290decb8c324e1e24f15d5d9802c3d9a2875
 
 Vue.use(VuePapaParse)
 
